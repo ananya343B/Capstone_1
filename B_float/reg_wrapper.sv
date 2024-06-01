2dea5130f265c28364c7192a6170cc0f29121ab5
ananya343B <142582353+ananya343B@users.noreply.github.com>
2024-06-01 11:16:52 +0530

    Create reg_wrapper.sv

---
+++ B_float/reg_wrapper.sv
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 30.05.2024 19:16:19
+// Design Name: 
+// Module Name: reg_wrapper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module reg_wrapper(
+    input clk,
+    input rst,
+    input [15:0] data_in,
+    output reg [15:0] reg_a,
+    output reg [15:0] reg_b,
+    output reg write_en
+);
+
+reg [1:0] state;
+reg [15:0] temp_data;
+
+always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+        state <= 2'b00; // Initialize state machine
+        reg_a <= 16'b0; // Initialize registers
+        reg_b <= 16'b0;
+        write_en <= 0;
+    end
+    else begin
+        case (state)
+            2'b00: begin
+                temp_data <= data_in;
+//                reg_a <= data_in;
+                state <= 2'b01;
+            end
+            2'b01: begin
+                reg_a <= temp_data;
+                reg_b <= data_in;
+                write_en <= 1;
+                state <= 2'b00;
+            end
+            default: state <= 2'b00; // Reset state to default
+        endcase
+    end
+end
+
+
+
+endmodule
