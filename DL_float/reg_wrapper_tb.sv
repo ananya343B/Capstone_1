8cf304ac59a0010af3819e9b9a9f417722463625
Nidz13 <166122007+Nidz13@users.noreply.github.com>
2024-06-03 21:32:38 +0530

    Update reg_wrapper_tb.sv

--- DL_float/reg_wrapper_tb.sv
+++ DL_float/reg_wrapper_tb.sv
@@ -25,27 +25,26 @@ module reg_wrapper_tb();
 
     initial begin
         rst=0;
-        #10
-        rst = 1;
-        #10
-      
-       
-        #10 data_in = 16'b1011111010100011;
-        #10 data_in = 16'b0100000001110011;
-        #10 data_in = 16'hC;
-        #10 data_in = 16'hD;
-        #10 data_in = 16'hE;
-        #10 data_in = 16'hF;
-        #10 data_in = 16'h10;
-        #10 data_in = 16'h11;
+        data_in=16'b0;
         
-        #100; 
+        #5rst = 1;
+      
+         #5data_in=16'hA;
+         #10
+         data_in=16'hB;
+         #10
+         data_in=16'hC;
+         #10
+         data_in=16'hD;
+        
+      
+        #60;
         $finish; 
     end
 
     
     initial begin
-      $monitor("At time %t, clk = %b, rst = %b, data_in = %h, reg_a = %h,                reg_b = %h, write_en = %b",$time, clk, rst, data_in, reg_a, reg_b,           write_en);
+      $monitor("At time %t, clk = %b, rst = %b, data_in = %h, reg_a = %h,reg_b = %h, write_en = %b",$time, clk, rst, data_in, reg_a, reg_b,write_en);
     end
 
 endmodule
