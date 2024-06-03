9b33e9e18460ecddba7d78407ec21a173f3237e1
Nidz13 <166122007+Nidz13@users.noreply.github.com>
2024-06-03 23:21:38 +0530

    Update dlfloat_adder_tb.sv

--- DL_float/dlfloat_adder_tb.sv
+++ DL_float/dlfloat_adder_tb.sv
@@ -1,50 +1,46 @@
 module dlfloat_adder_tb();
-
-  logic [15:0] a, b;
-  logic rst_n, clk;
-  logic [15:0] c;
-
   
-  dlfloat_adder dut (
-    .a(a),
-    .b(b),
-    .c(c),
-    .rst_n(rst_n),
-    .clk(clk)
-  );
-
- 
-  initial 
-  begin
-    clk = 0;
-    forever #5 clk = ~clk;
-  end
-
+  logic [15:0]a,b;
+  logic clk,rst_n;
+  logic [15:0]c;
   
-  initial  begin
-    logic [15:0] c1;
+  dlfloat_adder dut(.a(a),
+                   .b(b),
+                   .clk(clk),
+                   .rst_n(rst_n),
+                   .c(c)
+                  );
+  initial begin
+    clk=1;
     rst_n=0;
+    forever #5clk=~clk;
+  end
+  
+  initial begin
+    logic [15:0]c1;
+   
+    #5 rst_n=1;
     
-    #10 
-     // Test Case 1:same sign positve
+    #5// Test Case 1:same sign positve
     a = 16'b0011111010100011; // a=1.32
     b = 16'b0100000001110011; // b=2.45 
-     c1 =16'b0100000111000100; // c1= 3.765
+    c1 =16'b0100000111000100; // c1= 3.765
     #10;
     if (c == c1) 
       $display("Test case 1: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
      else 
        $display("Test case 1: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);
+    
     // Test Case 2:same sign negative
     a = 16'b1011111010100011; // a=-1.32
     b = 16'b1100000001110011; // b=-2.45 
-    c1 =16'b1100000111000100; // c1= 3.765
+    c1 =16'b1100000111000100; // c1= -3.765
     #10;
     if (c == c1) 
       $display("Test case 2: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
      else 
        $display("Test case 2: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);
-    // Test Case 3:different sign -ve<+ve
+     // Test Case 3:different sign -ve<+ve
     a = 16'b1011111010100011; // a=-1.32
     b = 16'b0100000001110011; // b=2.45 
     c1 =16'b0011111001000011; // c1= 1.13
@@ -53,8 +49,7 @@ module dlfloat_adder_tb();
       $display("Test case 3: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
      else 
        $display("Test case 3: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);
-    
-     // Test Case 4:different sign -ve>+ve
+    // Test Case 4:different sign -ve>+ve
     a = 16'b0011111010100011; // a=1.32
     b = 16'b1100000001110011; // b=-2.45 
    c1 =16'b1011111001000011; // c1= -1.13
@@ -63,30 +58,19 @@ module dlfloat_adder_tb();
       $display("Test case 4: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
      else 
        $display("Test case 4: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);
-
      // Test Case 5:one operand is zero
     a = 16'b0000000000000000; // a=0
     b = 16'b0100000001110011; // b=2.45 
-    c1 =16'b0100000001110011; // c1=2.45
+    c1 =16'b0000000000000000; // c1=0
     #10;
     if (c == c1) 
       $display("Test case 5: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
      else 
        $display("Test case 5: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);
-    
-     // Test Case 6:both operands are zero
-    a = 16'b0000000000000000; // a=0
-    b = 16'b0000000000000000; // b=0 
-   c1 =16'b0000000000000000; // c1=0
-    #10;
-    if (c == c1) 
-      $display("Test case 6: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
-     else 
-       $display("Test case 6: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);
      // Test Case 6:subnormals
-    a = 16'b0000000110101100; // a=0
+     a = 16'b0000000110101100; // a=subnormal
      b = 16'b0100000001110011; // b=2.45 
-    c1 =16'b0100000001110011; // c1=2.45
+    c1 = 16'b0000000000000000; // c1=0
     #10;
     if (c == c1) 
       $display("Test case 6: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
@@ -101,37 +85,34 @@ module dlfloat_adder_tb();
       $display("Test case 7: PASS. a = %b, b = %b, c = %b, expected:%b", a, b, c,c1);
      else 
        $display("Test case 7: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);
-    // Test Case 8:underflow and normal
+     // Test Case 8A:underflow and normal
     a = 16'b0000001000000000; // a=smallest +ve number
     b = 16'b0011111010100011; // b=1.32 
-   // c1 =16'b0000000000000000; // c1=Nan
     #10;
-   // if (c == c1) 
-    $display("Test case 8: a = %b, b = %b, c = %b", a, b, c);
-    /* else 
-       $display("Test case 8: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);*/
-    // Test Case 9:overrflow and normal
-    a = 16'b0111110111111110; // a=largest +ve number
-    b = 16'b0011111010100011; // b=1.32 
-   // c1 =16'b0000000000000000; // c1=Nan
+    $display("Test case 8A: a = %b, b = %b, c = %b", a, b, c);
+     // Test Case 8B:underflow and underflow
+    a = 16'b0000001000000000; // a=smallest +ve number
+    b = 16'b0000001000000000; // b=smallest +ve number
     #10;
-   // if (c == c1) 
-    $display("Test case 9: a = %b, b = %b, c = %b", a, b, c);
-    /* else 
-       $display("Test case 8: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);*/
-    // Test Case 10:overerflow and underflow
+    $display("Test case 8B: a = %b, b = %b, c = %b", a, b, c);
+     // Test Case 8C:underflow and overflow
     a = 16'b0000001000000000; // a=smallest +ve number
     b = 16'b0111110111111110; // b=largest +ve number 
-   // c1 =16'b0000000000000000; // c1=Nan
     #10;
-   // if (c == c1) 
-    $display("Test case 9: a = %b, b = %b, c = %b", a, b, c);
-    /* else 
-       $display("Test case 8: FAIL. a = %b, b = %b,  c = %b, expected: %b", a, b, c, c1);*/
+    $display("Test case 8C: a = %b, b = %b, c = %b", a, b, c);
+     // Test Case 8D:overflow and normal
+    a = 16'b0111110111111110; // a=largest +ve number
+    b = 16'b0011111010100011; // b=1.32 
+    #10;
+    $display("Test case 8D: a = %b, b = %b, c = %b", a, b, c);
+     // Test Case 8E:overflow and overflow
+    a = 16'b0111110111111110; // a=largest +ve number
+    b = 16'b0111110111111110; // b=largest +ve number
+    #10;
+    $display("Test case 8E: a = %b, b = %b, c = %b", a, b, c);
+   
     
-    $finish;
+    
+    #240 $finish;
   end
-
 endmodule
-
-
