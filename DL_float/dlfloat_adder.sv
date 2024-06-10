e7f7f413827f0fe8f519e72622e7f51925f42e9d
Nidz13 <166122007+Nidz13@users.noreply.github.com>
2024-06-11 00:31:26 +0530

    Update dlfloat_adder.sv

--- DL_float/dlfloat_adder.sv
+++ DL_float/dlfloat_adder.sv
@@ -19,19 +19,18 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output reg [15:0]c);
     
     reg    [15:0] Num_shift_80; 
     reg    [5:0]  Larger_exp_80,Final_expo_80;
-    reg    [8:0] Small_exp_mantissa_80,S_mantissa_80,L_mantissa_80,Large_mantissa_80,Final_mant_80;
-    reg    [9:0] Add_mant_80,Add1_mant_80;
+    reg    [9:0] Small_exp_mantissa_80,S_mantissa_80,L_mantissa_80,Large_mantissa_80;
+    reg    [8:0] Final_mant_80;
+    reg    [10:0] Add_mant_80,Add1_mant_80;
     reg    [5:0]  e1_80,e2_80;
     reg    [8:0] m1_80,m2_80;
     reg          s1_80,s2_80,Final_sign_80;
-    reg    [3:0]  renorm_shift_80;
+    reg    [8:0]  renorm_shift_80;
     integer signed   renorm_exp_80;
-    //reg           renorm_exp_80;
     reg    [15:0] c_80;
 
 
@@ -47,7 +46,7 @@ module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output
 	end 
 	end 
 			
-   // assign c = c_80;
+   
     
 
 
@@ -55,29 +54,24 @@ module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output
         //stage 1
 	e1_80 = a[14:9];
 	e2_80 = b[14:9];
-        m1_80 = a[8:0];
+    m1_80 = a[8:0];
 	m2_80 = b[8:0];
+      
 	s1_80 = a[15];
 	s2_80 = b[15];
-        // Nan and infinity check
-    if(e1_80 == 6'b111111 && e2_80 == 6'b111111 && m1_80 == 9'b111111111 && m2_80 == 9'b111111111)
-    begin
-        c_80 = 16'b1111111111111111;
-    end 
-    else
-    begin
+        
         if (e1_80  > e2_80) begin
-            Num_shift_80           = e1_80 - e2_80;              // number of mantissa shift
-            Larger_exp_80           = e1_80;                     // store lower exponent
-            Small_exp_mantissa_80  = m2_80;
-            Large_mantissa_80      = m1_80;
+            Num_shift_80           = e1_80 - e2_80;
+            Larger_exp_80          = e1_80;                     
+          Small_exp_mantissa_80    = {1'b1,m2_80};
+          Large_mantissa_80        = {1'b1,m1_80};
         end
         
         else begin
             Num_shift_80           = e2_80 - e1_80;
-            Larger_exp_80           = e2_80;
-            Small_exp_mantissa_80  = m1_80;
-            Large_mantissa_80      = m2_80;
+            Larger_exp_80          = e2_80;
+          Small_exp_mantissa_80    = {1'b1,m1_80};
+          Large_mantissa_80        = {1'b1,m2_80};
         end
 
 	if (e1_80 == 0 | e2_80 ==0) begin
@@ -86,13 +80,11 @@ module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output
 	else begin
 	    Num_shift_80 = Num_shift_80;
 	end
-	
-	
+      
         
         //stage 2
-        //if check both for normalization then append 1 and shift
+        // append 1 and shift
 	if (e1_80 != 0) begin
-            Small_exp_mantissa_80  = {1'b1,Small_exp_mantissa_80[8:1]};
 	    Small_exp_mantissa_80  = (Small_exp_mantissa_80 >> Num_shift_80);
         end
 	else begin
@@ -100,32 +92,26 @@ module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output
 	end
 
 	if (e2_80!= 0) begin
-            Large_mantissa_80      = {1'b1,Large_mantissa_80[8:1]};
+            Large_mantissa_80 = Large_mantissa_80;
 	end
 	else begin
 	    Large_mantissa_80 = Large_mantissa_80;
-	end
-
-        	//else do what to do for denorm field
-			
-
-        //stage 3
-                                                    //check if exponent are equal
+	end	
+              
+      //stage 3
+      //add the mantissas
+                                                    
             if (Small_exp_mantissa_80  < Large_mantissa_80) begin
-                //Small_exp_mantissa_80 = ((~ Small_exp_mantissa_80 ) + 1'b1);
-		//$display("what small_exp:%b",Small_exp_mantissa_80);
 		S_mantissa_80 = Small_exp_mantissa_80;
 		L_mantissa_80 = Large_mantissa_80;
             end
             else begin
-                //Large_mantissa_80 = ((~ Large_mantissa_80 ) + 1'b1);
-		//$display("what large_exp:%b",Large_mantissa_80);
 			
 		S_mantissa_80 = Large_mantissa_80;
 		L_mantissa_80 = Small_exp_mantissa_80;
              end       
-        //stage 4
-        //add the two mantissa's
+                       
+      Add_mant_80=11'b0;
 	
 	if (e1_80!=0 & e2_80!=0) begin
 		if (s1_80 == s2_80) begin
@@ -137,19 +123,66 @@ module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output
 	else begin
 		Add_mant_80 = L_mantissa_80;
 	end
+      
+	
          
 	//renormalization for mantissa and exponent
-	
-
+                              
 	//stage 5
-	// if e1==e2, no shift for exp
-        Final_expo_80 =  Larger_exp_80 + renorm_exp_80;
-	
-	Add1_mant_80 = Add_mant_80 << renorm_shift_80;
-
-	Final_mant_80 = Add1_mant_80[9:1];  	
-
+      if (Add_mant_80[10] ) begin
+		Add1_mant_80= Add_mant_80 >> 1;
+		renorm_exp_80 = 4'd1;
+	end
+      else begin 
+        if (Add_mant_80[9])begin
+		renorm_shift_80 = 0;
+		renorm_exp_80 = 0;		
+	end
+      else if (Add_mant_80[8])begin
+		renorm_shift_80 = 8'd1; 
+		renorm_exp_80 = -1;
+	end 
+      else if (Add_mant_80[7])begin
+		renorm_shift_80 = 8'd2; 
+		renorm_exp_80 = -2;		
+	end  
+      else if (Add_mant_80[6])begin
+		renorm_shift_80 = 8'd3; 
+		renorm_exp_80 = -3;		
+	end
+        else if (Add_mant_80[5])begin
+		renorm_shift_80 = 8'd4; 
+		renorm_exp_80 = -4;		
+	end
+        else if (Add_mant_80[4])begin
+		renorm_shift_80 = 8'd5; 
+		renorm_exp_80 = -5;		
+	end
+        else if (Add_mant_80[3])begin
+		renorm_shift_80 = 8'd6; 
+		renorm_exp_80 = -6;		
+	end
+        else if (Add_mant_80[2])begin
+		renorm_shift_80 = 8'd7; 
+		renorm_exp_80 = -7;		
+	end
+        else if (Add_mant_80[1])begin
+		renorm_shift_80 = 8'd8; 
+		renorm_exp_80 = -8;		
+	end
+        else if (Add_mant_80[0])begin
+		renorm_shift_80 = 8'd9; 
+		renorm_exp_80 = -9;		
+	end
+        Add1_mant_80 = Add_mant_80 << renorm_shift_80;
         
+      end
+     
+      
+      Final_expo_80 =  Larger_exp_80 + renorm_exp_80;
+	
+      Final_mant_80 = Add1_mant_80[8:0]; 
+
 	if (s1_80 == s2_80) begin
 		Final_sign_80 = s1_80;
 	end 
@@ -167,38 +200,12 @@ module dlfloat_adder(input clk,input rst_n, input [15:0]a, input [15:0]b, output
 			Final_sign_80 = s2_80;
 		end
 	end	
-	
+      if( a==1 | b==1) begin
+        c_80=1;
+      end
 	c_80 = (a==0 & b==0)?0:{Final_sign_80,Final_expo_80,Final_mant_80};
     end
-    end
-    always @(posedge clk)begin 
-    if (Add_mant_80[9] ) begin
-		renorm_shift_80 = 4'd1;
-		renorm_exp_80 = 4'd1;
-	end
-	else if (Add_mant_80[8])begin
-		renorm_shift_80 = 4'd2;
-		renorm_exp_80 = 0;		
-	end
-	else if (Add_mant_80[7])begin
-		renorm_shift_80 = 4'd3; 
-		renorm_exp_80 = -1;
-	end 
-	else if (Add_mant_80[6])begin
-		renorm_shift_80 = 4'd4; 
-		renorm_exp_80 = -2;		
-	end  
-	else if (Add_mant_80[5])begin
-		renorm_shift_80 = 4'd5; 
-		renorm_exp_80 = -3;		
-	end      
-	end 
     
-    
-//    always @(posedge clk) begin
-//            if(reset) begin
-//                Num_shift_80 <= #1 0;
-//            end
-//    end
-    
+  
 endmodule
+
